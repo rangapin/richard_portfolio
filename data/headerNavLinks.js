fe3c0d624e8604ecdb17c330141259fcf0278c53
rangapin <rangapin@gmx.com>
2022-12-11 18:46:41 -0600

    vercel deploy

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,7 @@
 const headerNavLinks = [
+  { href: '/projects', title: 'Projects' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Portfolio',
+  author: 'Richard Angapin',
+  headerTitle: 'Richard Angapin',
+  description: 'A Website created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
