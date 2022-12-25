037d29076fca58a09a89b2d2a84de827f97eb403
rangapin <rangapin@gmx.com>
2022-12-25 17:17:52 -0600

    added new post and project

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,12 +6,10 @@ const projectsData = [
     href: '/blog/freediving-school',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Cocktails Creole',
+    description: `Pick up a quick recipe for a nice homemade cocktail tonight.`,
+    imgSrc: '/static/images/cocktails-c.png',
+    href: 'https://cocktailscreole.vercel.app/',
   },
 ]
 
