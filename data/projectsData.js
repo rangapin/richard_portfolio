406fff32d7a37d99373d5bd09469689e3437eb55
rangapin <rangapin@gmx.com>
2024-03-14 23:27:03 +0200

    updated project data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Freediving School',
-    description: `This application compiles key points and video demonstrations of essential Freediving Skills. Learn today with Freediving School`,
+    description: `Embark on an immersive journey into the world of freediving with the Freediving School web application. Seamlessly blending functionality with aesthetics, this project serves as a gateway to mastering essential freediving skills. From general techniques to safety protocols and specialized disciplines, the Freediving School offers a comprehensive curriculum designed to equip enthusiasts with the knowledge and expertise needed to explore the depths with confidence.`,
     imgSrc: '/static/images/main-free.png',
     href: '/blog/freediving-school',
   },
@@ -9,17 +9,17 @@ const projectsData = [
     title: 'Random Quotes',
     description: `The Random Quotes project is a simple web application that generates and displays random quotes to the user. The application is built using HTML, CSS, and PHP, providing an elegant and minimalistic user interface.`,
     imgSrc: '/static/images/quotes.png',
-    href: 'https://richardangapin.xyz/',
+    href: '/blog/random-quotes',
   },
   {
     title: 'Blog card',
-    description: `This design piece was built using frontendmentor.io`,
+    description: `Explore the art of crafting captivating blog preview cards with this minimalist yet engaging web project. The Blog Preview Card project showcases a sleek and intuitive design, highlighting essential elements such as imagery, tags, publication dates, and author information. Whether you're a budding web developer looking to enhance your design skills or a seasoned professional seeking inspiration, this project offers a valuable opportunity to delve into the nuances of front-end web development and user interface design.`,
     imgSrc: '/static/images/blog-card.png',
     href: 'https://blog-card-main-ee1p8qxks-rangapin.vercel.app/',
   },
   {
     title: 'Recipe page',
-    description: `This design piece was built using frontendmentor.io`,
+    description: `The Simple Omelette Recipe Website is a responsive web application designed to provide users with a straightforward and easy-to-follow recipe for making a classic omelette. This project aims to offer a seamless user experience by presenting clear instructions, nutritional information, and visually appealing design elements.`,
     imgSrc: '/static/images/recipe.png',
     href: 'https://recipe-page-main-gamma.vercel.app/',
   },
