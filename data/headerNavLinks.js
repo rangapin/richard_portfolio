32d4257965cbd03fc6bb21501b7eaa575b96132c
Richard Angapin <richard.angapin@outlook.com>
2025-05-15 18:40:21 +0200

    all update
    Signed-off-by: Richard Angapin <richard.angapin@outlook.com>

--- data/headerNavLinks.js
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  // { href: '/about', title: 'About' },
-  { href: '/projects', title: 'Portfolio' },
-  /*   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' }, */
-]
-
-export default headerNavLinks
